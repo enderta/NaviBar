e4fcd46d3638526e4913b3cd4d2bbdf2d1f9cc8c
enderta <endertanver@gmail.com>
2022-10-17 13:10:38 +0100

    nav

--- src/components/navBarRouter/Movies.jsx
+++ src/components/navBarRouter/Movies.jsx
@@ -3,12 +3,35 @@ import React from 'react';
 const Movies = () => {
     return (
         <div>
-           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <div className="container-fluid">
-        <a className="navbar-brand" href="/">Home</a>
+            <header className="p-3 text-bg-dark">
+                <div className="container">
+                    <div
+                        className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                        <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                            <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
+                                <use xlinkHref="#bootstrap"/>
+                            </svg>
+                        </a>
+                        <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                            <li>
+                                <a href="/#" className="nav-link px-2 text-secondary">
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/series" className="nav-link px-2 text-white">
+                                    Series
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </header>
+
         </div>
-        </nav>
-        </div>
+
+
     );
 };
 
